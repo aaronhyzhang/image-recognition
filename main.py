9ac3921368f67912e077ab04f259db392db03957
aaronhyzhang <aaron11886z@gmail.com>
2023-10-10 15:45:40 -0500

    sldjfsadf

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Used to import al libraries, I used the facenet_pytorch library to detect faces and extract embeddings. Might change later since facenet feels like cheating.
 import os
 import cv2
 import torch
@@ -5,10 +6,12 @@ from facenet_pytorch import InceptionResnetV1, MTCNN
 from tqdm import tqdm
 from types import MethodType
 
+#Makes embedding of the face
 def encode(img):
     res = resnet(torch.Tensor(img))
     return res
 
+#Used to detect the bounds of the face
 def detect_box(self, img, save_path=None):
     # Detect the faces
     batch_boxes, batch_probs, batch_points = self.detect(img, landmarks=True)
@@ -21,24 +24,27 @@ def detect_box(self, img, save_path=None):
     faces = self.extract(img, batch_boxes, save_path)
     return batch_boxes, faces
 
-#Load the model
+#Load the pretrained model (OP)
 resnet = InceptionResnetV1(pretrained='vggface2').eval()
 mtcnn = MTCNN(
   image_size=224, keep_all=True, thresholds=[0.4, 0.5, 0.5], min_face_size=60
 )
 mtcnn.detect_box = MethodType(detect_box, mtcnn)
 
-#Reading the saved pictures
+#Reading the saved pictures, iterates through each file in the folder and saves the embeddings in a dictionary
 saved_pictures = "photos"
 total_faces = {}
 file = "facedetector.py"
+#What this is doing is splitting the file name and extension so it's assuming that it's jpg file and that the name is the name of the person.
 for file in os.listdir(saved_pictures):
     person_face, extension = os.path.splitext(file)
     if extension == 'jpg':
         img = cv2.imread(f'{saved_pictures}/{file}')
         cropped = mtcnn(img)
+        #This is why down here, it's saving the embedding of the face in the dictionary to the name.
         if cropped is not None:
             total_faces[person_face] = encode(cropped)[0, :]
+
 
 
 #Opening webcam
@@ -49,10 +55,12 @@ def detect(cam=0, thres=0.7):
         batch_boxes, cropped_images = mtcnn.detect_box(img0)
 
         if cropped_images is not None:
+            #Iterates through each detected face and it's corresponding bounding box on the webcam frame
             for box, cropped in zip(batch_boxes, cropped_images):
                 x, y, x2, y2 = [int(x) for x in box]
                 img_embedding = encode(cropped.unsqueeze(0))
                 detect_dict = {}
+                #Right now it's undetected, so might need more piectures to make it more accurate.
                 for k, v in total_faces.items():
                     detect_dict[k] = (v - img_embedding).norm().item()
                 if detect_dict:
@@ -63,6 +71,7 @@ def detect(cam=0, thres=0.7):
                 else:
                     min_key = 'Undetected'
                 
+                #Making the actual red box w font
                 cv2.rectangle(img0, (x, y), (x2, y2), (0, 0, 255), 2)
                 cv2.putText(
                   img0, min_key, (x + 5, y + 10), 
